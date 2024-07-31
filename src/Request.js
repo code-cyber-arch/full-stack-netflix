c679ae17aaebdb33476a4b0512a6535ed11acaa6
code-cyber-arch <archabdulrm@gmail.com>
2024-08-01 08:32:16 +1000

    Update env variable

--- src/Request.js
+++ src/Request.js
@@ -1,4 +1,4 @@
-const key = "a81414b83b96f7da69b4ca9418a365b6";
+const key = process.env.REACT_APP_IMDB_API_KEY;
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
